c7889eb6178221f13cc7429657d2a500c0948853
Charles Marcoin <charles.marcoin@gmail.com>
2022-08-18 10:33:37 +0200

    test ci1

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
+// Première route de du backend de l'application PPSMJ
 router.get('/', (req, res, next) => {
     res.json({message: "Hello from Mon Suivi Justice backend"})
 })
