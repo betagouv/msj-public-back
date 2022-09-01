436cf82913024ae3c2c76cbafc065a82aef7b0db
Charles Marcoin <charles.marcoin@gmail.com>
2022-09-01 10:00:41 +0200

    added eslint with standard guide and added scripts

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(usersRoutes)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log("Mon Suivi Justice back-end listening on", port);
-});
+  console.log('Mon Suivi Justice back-end listening on', port)
+})
