9455b339a687ee168dcb360ef1ae33848e2716a5
Charles Marcoin <charles.marcoin@gmail.com>
2022-09-01 15:44:47 +0200

    fix sequelize connection to pgqsql db, added jest conf

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const db = require('./models/index')
 // const bodyParser = require('body-parser')
 
 const usersRoutes = require('./routes/users-routes')
@@ -11,3 +12,14 @@ const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Mon Suivi Justice back-end listening on', port)
 })
+
+const testConnection = async () => {
+  try {
+    await db.sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+testConnection()
