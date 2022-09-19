e88ebee67851f3f51552ddd342f198c4208f47db
Charles Marcoin <charles.marcoin@gmail.com>
2022-09-19 21:03:52 +0200

    now getting the appointements from the rails backend

--- controllers/appointments-controller.js
+++ controllers/appointments-controller.js
@@ -1,30 +1,11 @@
-// const db = require('../models')
 const HttpError = require('../utils/http-error')
 const axios = require('axios')
-
-const APPOINTMENTS_LIST = [
-  {
-    id: '1',
-    date: 'Vendredi 15 Septembre 10h00',
-    place: 'SPIP 92'
-  },
-  {
-    id: '2',
-    date: 'Vendredi 15 Septembre 10h00',
-    place: 'SPIP 92'
-  },
-  {
-    id: '3',
-    date: 'Vendredi 15 Septembre 10h00',
-    place: 'SPIP 92'
-  }
-]
 
 const getUserAppointments = async (req, res, next) => {
   const msjId = req.params.msjId
   console.log(msjId)
 
-  const url = `${process.env.AGENTS_APP_DOMAIN}/convicts/${msjId}`
+  const url = `${process.env.AGENTS_APP_API_URL}/convicts/${msjId}`
   const username = process.env.AGENTS_APP_BASIC_AUTH_USERNAME
   const password = process.env.AGENTS_APP_BASIC_AUTH_PASSWORD
 
@@ -33,13 +14,20 @@ const getUserAppointments = async (req, res, next) => {
     'Content-type': 'application/json'
   }
 
-  const response = await axios.get(url, {
-    headers
-  })
+  let appointments
 
-  console.log(response)
+  try {
+    const response = await axios.get(url, {
+      headers
+    })
 
-  res.status(201).json(APPOINTMENTS_LIST)
+    appointments = response.data.appointments
+  } catch (err) {
+    const error = new HttpError("Une erreur s'est produite lors de la récupération des rendez-vous", 500)
+    return next(error)
+  }
+
+  res.status(201).json(appointments)
 }
 
 exports.getUserAppointments = getUserAppointments
