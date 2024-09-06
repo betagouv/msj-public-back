d16cdfe32c765f8c48bd89c86f13e888b249fa00
Damien Le Thiec <damien@dlet.fr>
2024-09-06 15:07:16 +0200

    First syntax improvement

--- src/utils/http-error.ts
+++ src/utils/http-error.ts
@@ -1,5 +1,7 @@
 export default class HttpError extends Error {
+  public originalError : Error | undefined
   constructor (public message: string, public code: number, public originalError?: Error, public sendToSentry?: boolean) {
     super(message)
+    this.originalError =  originalError instanceof Error ? originalError : undefined
   }
 }
